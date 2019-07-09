dc0a8ede2b7266a7e789f889bc579313fbceb0d0
James McRae <james.mcraejr@gmail.com>
2019-07-09 11:10:12 -0400

    switched react-select-search to react-select. able to set state of component based on selection.

--- src/components/MaterialConverter.js
+++ src/components/MaterialConverter.js
@@ -1,59 +1,277 @@
 import React, { Component } from "react";
-import SelectSearch from "react-select-search";
+import Select from "react-select";
 import "../css/MaterialConverter.css";
 
+const groupStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between"
+};
+
+const formatGroupLabel = data => (
+  <div style={groupStyles}>
+    <span>{data.label}</span>
+  </div>
+);
 
 class MaterialConverter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      resource1: "",
-      resource1Id: 0
+      count: 0
     };
   }
 
-  searchOptions() {
-    let resourceTypes = [];
-    for (let i = 0; i < this.props.resources.length; i++) {
-      let type = this.props.resources[i].type;
-      if (resourceTypes.indexOf(type) === -1) {
-        resourceTypes.push(type);
+  createOptions = () => {
+    const resources = this.props.resources;
+    let fiberOptions = [];
+    let thatchOptions = [];
+    let woodOptions = [];
+    let sugarOptions = [];
+    let coalOptions = [];
+    let flintOptions = [];
+    let stoneOptions = [];
+    let metalOptions = [];
+    let alloysOptions = [];
+    let crystalOptions = [];
+    let gemOptions = [];
+    let saltOptions = [];
+    let hideOptions = [];
+    let ketOptions = [];
+    let coralOptions = [];
+    let pasteOptions = [];
+    let oilOptions = [];
+    let mythosOptions = [];
+    let craftableOptions = [];
+
+    for (let i = 0; i < resources.length; i++) {
+      if (resources[i].resource_type_id === 1) {
+        fiberOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 2) {
+        thatchOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 3) {
+        woodOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 4) {
+        sugarOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 5) {
+        coalOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 6) {
+        flintOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 7) {
+        stoneOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 8) {
+        metalOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 9) {
+        alloysOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 10) {
+        crystalOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 11) {
+        gemOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 12) {
+        saltOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 13) {
+        hideOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 14) {
+        ketOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 15) {
+        coralOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 16) {
+        pasteOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 17) {
+        oilOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 18) {
+        mythosOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
+      } else if (resources[i].resource_type_id === 19) {
+        craftableOptions.push({
+          value: resources[i].name,
+          label: resources[i].name
+            .split(" ")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" "),
+          goldValue: resources[i].gold_value,
+          resourceId: resources[i].id
+        });
       }
     }
-    let options = [];
-    resourceTypes.forEach(type => {
-      let items = [];
-      for (let i = 0; i < this.props.resources.length; i++) {
-        if (this.props.resources[i].type === type) {
-          items.push({
-            name: this.props.resources[i].name,
-            value: this.props.resources[i].id
-          });
-        }
-      }
-      let groups = { type: "group", name: type, items: items };
-      options.push(groups);
-    });
-    return options;
-  }
+    const groupedOptions = [
+      { label: "Fibers", options: fiberOptions },
+      { label: "Thatch", options: thatchOptions },
+      { label: "Wood", options: woodOptions },
+      { label: "Sugars", options: sugarOptions },
+      { label: "Coal", options: coalOptions },
+      { label: "Flint", options: flintOptions },
+      { label: "Stone", options: stoneOptions },
+      { label: "Metals", options: metalOptions },
+      { label: "Alloys", options: alloysOptions },
+      { label: "Crystal", options: crystalOptions },
+      { label: "Gems", options: gemOptions },
+      { label: "Salt", options: saltOptions },
+      { label: "Hide", options: hideOptions },
+      { label: "Karatanoid", options: ketOptions },
+      { label: "Coral", options: coralOptions },
+      { label: "Paste", options: pasteOptions },
+      { label: "Oil", options: oilOptions },
+      { label: "Mythos", options: mythosOptions },
+      { label: "Craftables", options: craftableOptions }
+    ];
+    return groupedOptions;
+  };
 
-  handleInputChange = e => {
-    console.log(e)
-    this.setState({ resource1: e.name });
+  handleInputChange = option => {
+    this.setState({ [`resource${this.state.count}`]: option });
   };
 
   render() {
     return (
-      <div>
-        <SelectSearch
-          options={this.searchOptions()}
-          value={this.state.resource1}
-          name="resource1"
-          placeholder="Search for Resource"
-          // onChange={this.handleInputChange}
-        />
-        <input></input>
-      </div>
+      <Select
+        options={this.createOptions()}
+        formatGroupLabel={formatGroupLabel}
+        onChange={this.handleInputChange}
+      />
     );
   }
 }
